4521d02201d113332bb1b06d93131cc611c49810
Varun <sharma.varun76@yahoo.in>
2021-03-20 22:14:32 +0530

    Eslint Added
    Added the eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'plugin:vue/base',
+    'plugin:vue/recommended',
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/no-v-html',
+    'plugin:vue/attributes-order',
+    'plugin:vue/component-tags-order',
+    'plugin:vue/no-multiple-slot-args',
+    'plugin:vue/no-lone-template',
+    'plugin:vue/order-in-components',
+    'plugin:vue/this-in-template',
+    'plugin:vue/camelcase',
+    'plugin:vue/eqeqeq'
+  ]
+}
