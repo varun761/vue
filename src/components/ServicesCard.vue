3909d2a1e29cab0f0e760be94b35f5e0c2cf072a
varun761 <sharma.varun76@yahoo.in>
2021-04-13 21:31:07 +0530

    Added: Services card

--- src/components/ServicesCard.vue
+++ src/components/ServicesCard.vue
@@ -11,17 +11,65 @@ export default {
     },
     render(h) {
         const services = this.sections.map((el) => {
+            const childElement = []
+            const description = el.description || null
+            const features = Object.prototype.hasOwnProperty.call(el, 'features') && el.features && Array.isArray(el.features) ? el.features : []
+            if (description) {
+                /*
+                * Description
+                */
+                const descriptionElement = h('p', {
+                    class: {
+                        'font-weight-lighter': true,
+                        'card-paragraph': true
+                    },
+                    domProps: {
+                        innerHTML: el.description 
+                    }
+                })
+                childElement.push(descriptionElement)
+            }
+            /*
+            * Features
+            */
+            if (features) {
+                const featuresList = []
+                features.map((featureListElement) => h('li', {
+                    domProps: {
+                        innerHTML: featureListElement
+                    }
+                })).map((featureItems) => featuresList.push(featureItems))
+                if (featuresList.length > 0) {
+                    const featureList = h('ul', {
+                        class: {
+                            'feature-list': true 
+                        }
+                    }, featuresList)
+                    childElement.push(featureList)
+                }
+            }
+            /*
+            * Card element
+            */
             const cardElement = h('b-card', {
                 props: {
                     title: el.title
                 },
+                class: {
+                    serviceCard: true,
+                    'text-center': true
+                },
                 scopedSlots: {
-                    default: () => el.innerText
+                    default: () => h('div', {}, [childElement])
                 }
             })
+            /*
+            * Row
+            */
             return h('b-col', {
                 props: {
-                    col: 12,
+                    cols: 12,
+                    sm: 6,
                     md: 4
                 },
                 class: {
@@ -34,3 +82,24 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.serviceCard {
+    border-color: #05386B;
+    border-radius: 1rem;
+    .card-title {
+        color: #05386B;
+    }
+    .card-paragraph {
+        font-size: 14px;
+        line-height: 24px;
+    }
+    .feature-list {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        font-weight: lighter;
+        line-height: 24px;
+    }
+}
+</style>
