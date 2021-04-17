715b2f647cd2eb360be7381a6b0efa1544c9a3b7
varun761 <sharma.varun76@yahoo.in>
2021-04-17 10:43:33 +0530

    Vuelidate and firebase added
    Add the vuelidate for form validations and firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Firebase App is always required and must be first
+import firebase from 'firebase/app'
+
+// Add additional services that you want to use
+import 'firebase/auth'
+import 'firebase/firestore'
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+}
+export const Firebase = firebase.initializeApp(config)
+// export const FirebaseAuth = firebase.auth
+// export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp()
+// export const firestore = Firebase.firestore()
+// Firebase.firestore().enablePersistence({ synchronizeTabs: true })
--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ import router from './router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuelidate from 'vuelidate'
+import { Firebase } from './firebase.config'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(Vuelidate)
+let app
+Firebase.auth().onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h => h(App),
+    }).$mount('#app')
+  }
+})
